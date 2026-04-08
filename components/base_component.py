08ba99b387e9890fd109b83e256bf4b79664ee97
MNOGOIGRAAL <artur-ismailov-01@mail.ru>
2026-04-08 21:50:12 +0500

    Add autotests 5

---
+++ components/base_component.py
@@ -0,0 +1,7 @@
+from playwright.sync_api import Page, Locator
+
+
+class BaseComponent:
+    def __init__(self, page: Page, wrapper: Locator):
+        self.page = page
+        self.wrapper: Locator = wrapper
