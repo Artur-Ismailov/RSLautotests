762a1271b9ec04e5a3b6f0ef30e4136c5363dc84
MNOGOIGRAAL <artur-ismailov-01@mail.ru>
2026-04-12 18:28:36 +0500

    Ref tests PERS_ACC

--- tests/test_RSL_PERS_ACC_01.py
+++ tests/test_RSL_PERS_ACC_01.py
@@ -15,23 +15,23 @@ def test_RSL_PERS_ACC_01(page: Page):
 
         assert "https://passport.rusneb.ru/auth/realms/RSL/protocol/openid-connect/auth?response_type=code&redirect_uri=https%3A%2F%2Fwww.rsl.ru%2Flogin&client_id=rsl.ru&scope=rsl_udb_profile+openid" in login_page.url
 
-        expect(login_page.get_username_input()).to_be_visible()
-        expect(login_page.get_password_input()).to_be_visible()
-        expect(login_page.get_login_button()).to_be_visible()
+        expect(login_page.get_username_input().locator).to_be_visible()
+        expect(login_page.get_password_input().locator).to_be_visible()
+        expect(login_page.get_login_button().locator).to_be_visible()
 
     with allure.step("2. В поле ввода «Адрес электронной почты или номер читательского билета» ввести «invalid@mail.ru»"):
         login_page.fill_username_input("invalid@mail.ru")
 
-        expect(login_page.get_username_input()).to_have_value("invalid@mail.ru", timeout=5000)
+        expect(login_page.get_username_input().locator).to_have_value("invalid@mail.ru", timeout=5000)
 
     with allure.step("3. В поле ввода «Пароль» ввести «password»"):
         login_page.fill_password_input("password")
 
-        expect(login_page.get_password_input()).to_have_value("password", timeout=5000)
+        expect(login_page.get_password_input().locator).to_have_value("password", timeout=5000)
 
     with allure.step("4. Кликнуть на кнопку [Войти]"):
         login_page.click_login_button()
 
-        expect(login_page.get_error_message()).to_be_visible(timeout=10000)
-        expect(login_page.get_error_message()).to_contain_text("Неправильное имя пользователя или пароль", timeout=5000)
-        expect(login_page.get_error_message()).to_contain_text("Или данные были удалены согласно 152-ФЗ", timeout=5000)
+        expect(login_page.get_error_message().locator).to_be_visible(timeout=10000)
+        expect(login_page.get_error_message().locator).to_contain_text("Неправильное имя пользователя или пароль", timeout=5000)
+        expect(login_page.get_error_message().locator).to_contain_text("Или данные были удалены согласно 152-ФЗ", timeout=5000)
--- tests/test_RSL_PERS_ACC_02.py
+++ tests/test_RSL_PERS_ACC_02.py
@@ -15,19 +15,19 @@ def test_RSL_PERS_ACC_02(page: Page):
 
         assert "https://passport.rusneb.ru/auth/realms/RSL/protocol/openid-connect/auth?response_type=code&redirect_uri=https%3A%2F%2Fwww.rsl.ru%2Flogin&client_id=rsl.ru&scope=rsl_udb_profile+openid" in login_page.url
 
-        expect(login_page.get_username_input()).to_be_visible()
-        expect(login_page.get_password_input()).to_be_visible()
-        expect(login_page.get_login_button()).to_be_visible()
+        expect(login_page.get_username_input().locator).to_be_visible()
+        expect(login_page.get_password_input().locator).to_be_visible()
+        expect(login_page.get_login_button().locator).to_be_visible()
 
     with allure.step("2. В поле ввода «Адрес электронной почты или номер читательского билета» ввести «qatester12348765@mail.ru»"):
         login_page.fill_username_input("qatester12348765@mail.ru")
 
-        expect(login_page.get_username_input()).to_have_value("qatester12348765@mail.ru", timeout=5000)
+        expect(login_page.get_username_input().locator).to_have_value("qatester12348765@mail.ru", timeout=5000)
 
     with allure.step("3. В поле ввода «Пароль» ввести «QAtester1234»"):
         login_page.fill_password_input("QAtester1234")
 
-        expect(login_page.get_password_input()).to_have_value("QAtester1234", timeout=5000)
+        expect(login_page.get_password_input().locator).to_have_value("QAtester1234", timeout=5000)
 
     with allure.step("4. Кликнуть на кнопку [Войти]"):
         login_page.click_login_button()
